2411c5b658105c474080d5703224050606943c45
shahabrar7746 <73398955+shahabrar7746@users.noreply.github.com>
2021-04-08 20:56:45 +0530

    appropriate code added

--- Stocks2.java
+++ Stocks2.java
@@ -2,33 +2,38 @@
 import java.util.*;
 class Stocks2 {
     public static void main(String[] args) {
-   Scanner sc = new Scanner(System.in);
-//Defining Array and its value
- int array[] = {20,90,98,56,35,84,73,12,56,9,56,89,34};
-;
-	int max = array[0];
-	int min = array[0];
-	int location = 0;
-	int location1 = 0;
-	//Finding maximum and minimum stock for days
-	for(int i = 0;i<array.length;i++){
-	    if(max < array[i]){
-	        max = array[i];
-	        location = i;
-	    }if(min > array[i]){
-	        min = array[i];
-	        location1 = i;
-	    }
-	   
-	    
-	    
-	} location = location + 1;
-	location1 = location1 + 1;
-	//Showing maximum and minimum stock values
-	 System.out.println("Max stock value is " + max + " at " + location + " "  );
-	
-	     System.out.println("Min stock value is " + min + " at " + location1 + " ");
-	     int profit = max - min;
-	     System.out.println("Profit would be " + profit);
+  Scanner sc = new Scanner(System.in);
+	    //defining size of array
+        int n = sc.nextInt();
+        int array[] = new int[n];
+	    //Defining values for array
+        for(int i = 0;i<array.length;i++){
+            array[i] = sc.nextInt();
+        }
+        int max = 0;
+        int min = 0;
+        int location = 0;
+        int location1 = 0;
+	    //Calculating max and min stocks
+        for(int i = 0;i<array.length;i++){
+            for(int j =0;j<array.length;j++){
+                max = array[i];
+                min = array[i];
+                if(max<array[j]){
+                    max = array[j];
+                    location = i;
+                    
+                }if(min>array[j]){
+                    min = array[j];
+                    location = j;
+                }
+                
+            }
+            if(location1 < location){
+                break;
+            }
+        }
+	    //Printing profits
+        System.out.println(max - min);
 }
 }
